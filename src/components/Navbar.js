45c4e35f931d0bb265b36b64031e2d5ef6868da9
Sardar Rafay Alam <rafayy99@gmail.com>
2021-11-13 21:37:15 -0600

    Final Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo1.png";
 import { Link } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder";
 import "../styles/Navbar.css";
